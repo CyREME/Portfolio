4ddce014ed913b46f8ad86b9e86ea82413b5e9df
Emeric <102652005+CyREME@users.noreply.github.com>
2026-02-12 15:51:09 +0400

    Change server listen address to '0.0.0.0'
    Updated server to listen on '0.0.0.0' and modified log message.

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Infomaniak nous donne un port dynamique, sinon on utilise 3000 en local
+// Infomaniak fournit le port, sinon 3000
 const PORT = process.env.PORT || 3000;
 
-// On dit à Node de servir les fichiers du dossier "public"
 app.use(express.static('public'));
 
-// Pour toutes les routes, on renvoie l'index.html (Single Page App friendly)
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Serveur lancé sur le port ${PORT}`);
+// MODIFICATION ICI : On ajoute '0.0.0.0'
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`✅ Serveur lancé sur le port ${PORT}`);
 });
